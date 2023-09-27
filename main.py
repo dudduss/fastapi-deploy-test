dcd3c0fa11a2f0206ec6f35c4945a3056d0e9d64
dudduss <sampathh.s@gmail.com>
2023-09-27 18:52:45 -0400

    working

--- main.py
+++ main.py
@@ -6,21 +6,34 @@ import random
 import numpy as np
 from bs4 import BeautifulSoup
 import re
+import chromadb
 
 app = FastAPI(title="Hebbia v0", description="An early version of the Hebbia AI app")
 
-embedding_mappings = {}
-doc_metadata_mappings = {}
+## Local Storage
+# embedding_mappings = {}
+# doc_metadata_mappings = {}
+
 model = SentenceTransformer("sentence-transformers/msmarco-MiniLM-L-6-v3")
 MAX_TOKEN_SIZE = 100
+
+chroma_client = chromadb.Client()
+collection = chroma_client.create_collection(name="financial_passages")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, world!"}
+    collection.add(
+        documents=["This is a document", "This is another document"],
+        metadatas=[{"source": "my_source"}, {"source": "my_source"}],
+        ids=["id1", "id2"],
+    )
+
+    results = collection.query(query_texts=["This is a query document"], n_results=2)
+    return results
 
 
-@app.post("/upload")
+@app.post("/documents")
 async def upload_file(file: UploadFile):
     def get_token_length(sentence):
         return len(sentence.split(" "))
@@ -37,6 +50,36 @@ async def upload_file(file: UploadFile):
         chunk_strings = [" ".join(chunk_arr) for chunk_arr in chunks_arrs]
         return chunk_strings
 
+    def get_file_type(filename):
+        if filename.endswith(".html"):
+            return "html"
+        elif filename.endswith(".txt"):
+            return "txt"
+        else:
+            return "unknown"
+
+    def get_source():
+        sources = ["sharepoint", "drive", "dropbox", "file_upload", "email"]
+        return random.choice(sources)
+
+    def get_company(filename):
+        companies = [
+            "amazon",
+            "apple",
+            "ford",
+            "netflix",
+            "nikola",
+            "salesforce",
+            "tesla",
+            "walt disney",
+        ]  # hardcoded, can change to LLM based approach later
+
+        filename = filename.lower()
+        for company in companies:
+            if company in filename:
+                return company
+        return "unknown"
+
     # Get random doc_id
     doc_id = random.randint(0, 1000000)
 
@@ -50,7 +93,7 @@ async def upload_file(file: UploadFile):
         # Read the content
         contents = await file.read()
 
-        # Separate logic if html
+        # Separate logic when html
         if file.filename.endswith(".html"):
             soup = BeautifulSoup(contents, "html.parser")
             contents = soup.get_text()
@@ -95,28 +138,46 @@ async def upload_file(file: UploadFile):
 
         # Encode the chunks and store in local storage
         embeddings = encode_chunks(chunks)
-        for i in range(0, len(embeddings)):
-            embedding_key = tuple(embeddings[i])
-            chunk = chunks[i]
-            value = {
+        embeddings = [list(embedding) for embedding in embeddings]
+
+        # TODO - use hash of chunk + filename + date_uploaded to allow uploads of same file
+        ids = [str(hash(chunk)) for chunk in chunks]
+        metadatas = [
+            {
                 "doc_id": doc_id,
-                "passage": chunk,
-            }
-            embedding_mappings[embedding_key] = value
-
-        # Store the metadata
-        doc_metadata_mappings[doc_id] = {
-            "filename": file.filename,
-            "num_chunks": len(chunks),
-        }
-
-        return {
-            "chunks": chunks,
-            "metadata": {
                 "filename": file.filename,
-                "num_chunks": len(chunks),
-            },
-        }
+                "file_type": get_file_type(file.filename),
+                "description": "",
+                "source": get_source(),
+                "company": get_company(file.filename),
+            }
+            for chunk in chunks
+        ]
+
+        collection.add(
+            documents=chunks,
+            # embeddings=embeddings,
+            metadatas=metadatas,
+            ids=ids,
+        )
+
+        ## Local Storage
+        # for i in range(0, len(embeddings)):
+        #     embedding_key = tuple(embeddings[i])
+        #     chunk = chunks[i]
+        #     value = {
+        #         "doc_id": doc_id,
+        #         "passage": chunk,
+        #     }
+        #     embedding_mappings[embedding_key] = value
+
+        # # Store the metadata
+        # doc_metadata_mappings[doc_id] = {
+        #     "filename": file.filename,
+        #     "num_chunks": len(chunks),
+        # }
+
+        return {"chunks": chunks, "metadata": metadatas[0]}
 
     except Exception as e:
         print('Failed to upload file: "' + file.filename + "with error " + str(e) + '"')
@@ -125,11 +186,6 @@ async def upload_file(file: UploadFile):
 
 class SearchQuery(BaseModel):
     query: str
-
-
-@app.get("/mappings")
-async def mappings():
-    print(embedding_mappings)
 
 
 @app.post("/ingest/bulk")
@@ -146,32 +202,51 @@ async def ingest_bulk(files: list[UploadFile]):
     return results
 
 
-@app.post("/search")
+@app.post("/passages/search")
 async def search(query: SearchQuery):
-    results = []
-    MAX_RESULTS_SIZE = 5
-    query_embedding = model.encode([query.query])[0]
-    for embedding_key in embedding_mappings.keys():
-        ## convert embedding_key tuple into np array
-        embedding = np.array(embedding_key)
-        ## calculate cosine similarity
-        cosine_similarity = np.dot(query_embedding, embedding) / (
-            np.linalg.norm(query_embedding) * np.linalg.norm(embedding)
-        )
+    result = collection.query(query_texts=[query.query], n_results=5)
+    # print("chrroma result: ", result)
+    passages = [
+        {
+            "id": result["ids"][0][i],
+            "passage": result["documents"][0][i],
+            "metadata": result["metadatas"][0][i],
+            "distance": result["distances"][0][i],
+        }
+        for i in range(0, len(result["ids"][0]))
+    ]
+    return passages
 
-        embedding_value = embedding_mappings[embedding_key]
-        results.append(
-            {
-                "confidence": round(float(cosine_similarity), 5),
-                "doc_id": embedding_value["doc_id"],
-                "passage": embedding_value["passage"],
-                "metadata": doc_metadata_mappings[embedding_value["doc_id"]],
-            }
-        )
 
-    ## sort by cosine similarity in descending order
-    results.sort(key=lambda x: x["confidence"], reverse=True)
+@app.post("/db/clear")
+async def clear_db():
+    chroma_client.reset()
 
-    ## return the top 5 results
-    results = results[:MAX_RESULTS_SIZE]
-    return results
+    ## Local Storage approach
+    # results = []
+    # MAX_RESULTS_SIZE = 5
+    # query_embedding = model.encode([query.query])[0]
+    # for embedding_key in embedding_mappings.keys():
+    #     ## convert embedding_key tuple into np array
+    #     embedding = np.array(embedding_key)
+    #     ## calculate cosine similarity
+    #     cosine_similarity = np.dot(query_embedding, embedding) / (
+    #         np.linalg.norm(query_embedding) * np.linalg.norm(embedding)
+    #     )
+
+    #     embedding_value = embedding_mappings[embedding_key]
+    #     results.append(
+    #         {
+    #             "confidence": round(float(cosine_similarity), 5),
+    #             "doc_id": embedding_value["doc_id"],
+    #             "passage": embedding_value["passage"],
+    #             "metadata": doc_metadata_mappings[embedding_value["doc_id"]],
+    #         }
+    #     )
+
+    # ## sort by cosine similarity in descending order
+    # results.sort(key=lambda x: x["confidence"], reverse=True)
+
+    # ## return the top 5 results
+    # results = results[:MAX_RESULTS_SIZE]
+    # return results
